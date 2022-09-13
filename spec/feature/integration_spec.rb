bdbed3acdda5b5c1647577a87b340719a8e9130a
root <root@localhost>
2022-09-13 05:00:02 +0000

    added more tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -4,9 +4,34 @@ require 'rails_helper'
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
+    fill_in 'Title', with: 'The Fellowship of the Ring'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('The Fellowship of the Ring')
+  end
+
+RSpec.describe 'Setting the author', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Author', with: 'J. R. R. Tolkien'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('J. R. R. Tolkien')
+  end
+RSpec.describe 'Setting the price', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Price', with: '9.99'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('9.99')
+  end
+RSpec.describe 'Setting the published date', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'PublishedDate', with: '07/29/1954'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('07/29/1954')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'The Fellowship of the Ring')
   end
 
   it 'is valid with valid attributes' do
@@ -14,4 +14,47 @@ RSpec.describe Book, type: :model do
     subject.title = nil
     expect(subject).not_to be_valid
   end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(author: 'J. R. R. Tolkien')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(price: '9.99')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(publishedDate: '07/29/1954')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.publishedDate = nil
+    expect(subject).not_to be_valid
+  end
+
 end
