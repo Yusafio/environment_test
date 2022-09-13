d3258bb249538ba1ebd7ca39e2e4bf0bb5669903
root <root@localhost>
2022-09-13 07:16:37 +0000

    testing changes

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,37 +1,25 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
+def select_date(date, options = {})
+    field = options[:from]
+    base_id = find(:xpath, ".//label[contains(.,'#{field}')]")[:for]
+    year, month, day = date.split(',')
+    select year,  :from => "#{base_id}_1i"
+    select month, :from => "#{base_id}_2i"
+    select day,   :from => "#{base_id}_3i"
+  end
+
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'The Fellowship of the Ring'
+    fill_in 'Author', with: 'J. R. R. Tolkien'
+    fill_in 'Price', with: '9.99'
+    select_date("1997,June,26", :from => "Published date")
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('The Fellowship of the Ring')
   end
 
-RSpec.describe 'Setting the author', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Author', with: 'J. R. R. Tolkien'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('J. R. R. Tolkien')
-  end
-RSpec.describe 'Setting the price', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Price', with: '9.99'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('9.99')
-  end
-RSpec.describe 'Setting the published date', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'PublishedDate', with: '07/29/1954'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('07/29/1954')
-  end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'The Fellowship of the Ring')
+    described_class.new(title: 'The Fellowship of the Ring', author: "J. R. R. Tolkien", price: "9.99", publishedDate: "1954-07-07")
   end
 
   it 'is valid with valid attributes' do
@@ -12,48 +12,6 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
-    expect(subject).not_to be_valid
-  end
-
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(author: 'J. R. R. Tolkien')
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without a name' do
-    subject.author = nil
-    expect(subject).not_to be_valid
-  end
-
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(price: '9.99')
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without a name' do
-    subject.price = nil
-    expect(subject).not_to be_valid
-  end
-
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(publishedDate: '07/29/1954')
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without a name' do
-    subject.publishedDate = nil
     expect(subject).not_to be_valid
   end
 
