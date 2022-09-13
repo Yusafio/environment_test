c0d7d0b691725d7dee73cf8d36c12a21bc61fefa
root <root@localhost>
2022-09-13 08:22:49 +0000

    test changes all should pass

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'The Fellowship of the Ring'
     fill_in 'Author', with: 'J. R. R. Tolkien'
     fill_in 'Price', with: '9.99'
-    fill_in 'Publisheddate', with: '1954-07-29'
+    fill_in 'Published', with: '1954-07-29'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('The Fellowship of the Ring')
