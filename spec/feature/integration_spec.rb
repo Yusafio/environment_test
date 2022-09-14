d8502e4d7f0c8a04e79b09053ba254d8d9b9d01b
root <root@localhost>
2022-09-14 04:01:44 +0000

    changed back to date dropdowns and implemented date dropdown tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,13 +1,22 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
+def select_date(date, options = {})
+    field = options[:from]
+    base_id = find(:xpath, ".//label[contains(.,'#{field}')]")[:for]
+    year, month, day = date.split(',')
+    select year,  :from => "#{base_id}_1i"
+    select month, :from => "#{base_id}_2i"
+    select day,   :from => "#{base_id}_3i"
+  end
+
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'The Fellowship of the Ring'
     fill_in 'Author', with: 'J. R. R. Tolkien'
     fill_in 'Price', with: '9.99'
-    fill_in 'Published', with: '1954-07-29'
+    select_date("1954,July,29", :from => "Published")
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('The Fellowship of the Ring')
